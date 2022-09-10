e60c2aa46ffd8f9bf5d721196fe4e85c92b25a9f
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2022-09-10 23:01:12 +0700

    tugas 2 dah bisa

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_wishlist = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Fattah',
+        'npm' : '2106653741'
+    }
+
+    return render(request, "katalog.html", context)

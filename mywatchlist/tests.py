51e1bf703c487256be167d587b23e05a620689f4
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2022-09-22 07:20:16 +0700

    checklist all tasks

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import re
+from django.test import TestCase, Client
 
 # Create your tests here.
+from django.test import TestCase, Client
+
+# Create your tests here.
+class MyWatchListTest(TestCase):
+    def test_html(self):
+        response = Client().get('/mywatchlist/html/')
+        res = self.assertEqual(response.status_code, 200)
+        return res
+    
+    def test_xml(self):
+        response = Client().get('/mywatchlist/xml/')
+        res = self.assertEqual(response.status_code, 200)
+        return res
+    
+    def test_json(self):
+        response = Client().get('/mywatchlist/json/')
+        res = self.assertEqual(response.status_code, 200)
+        return res
